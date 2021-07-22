b78e188fb669725394dff48f79e6eb3bd871c74f
YnothnaFr <aazstudio01@gmail.com>
2021-07-22 10:44:15 +0200

    Trying pointer with small hour exercice

--- main.c
+++ main.c
@@ -1,47 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
+void decompteTotal(int *hour, int *mins);
+
+// by defining pointer
+//int main() {
+//    int hour, mins = 0;
+//    int *pointerHour = &hour;
+//    int *pointerMin = &mins;
+//
+//    printf("\n Mins : \n");
+//    scanf("%d", &mins);
+//
+//    decompteTotal(pointerHour, pointerMin);
+//
+//    printf("%d Heures et %d minutes", hour, mins);
+//}
+
+// by reference
 int main() {
-    // var init
-    const int MIN = 0;
-    const int MAX = 100;
-    int retry, gameType = 0;
-    int randomNumber, userNumber, round = 0;
-    srand(time(NULL));
+    int hour, mins = 0;
 
-    do {
-        printf("0 : Solo \n"
-               "1 : 2 joueurs \n");
-        scanf_s("%d", &gameType);
+    printf("\n Mins : \n");
+    scanf("%d", &mins);
 
-        if (gameType) {
-            printf("Rentrez une nombre compris entre 1 et 100 \n");
-            scanf_s("%d", &randomNumber);
-        } else {
-            randomNumber = (rand() % (MAX - MIN + 1)) + MIN;
-            printf("%d \n", randomNumber);
-        }
+    decompteTotal(&hour, &mins);
 
-        printf("Arriverez vous a trouver le bon nombre ?\n");
-        printf("Compris entre 0 et 100\n");
-
-        do {
-            round++;
-            printf("Nombre :");
-            scanf_s("%d", &userNumber);
-            printf("\n");
-            printf("Coup numero %d : \n", round);
-            if (userNumber == randomNumber) {
-                printf("Bien joue ! Cetais bien le bon nombre :)\n");
-            } else {
-                printf("Eh non ! Retente ta chance !\n");
-            }
-        } while (userNumber != randomNumber);
-        printf("On rejoue ?");
-        scanf_s("%d", &retry);
-        round = 0;
-    } while (retry);
-
-    return 0;
+    printf("%d Heures et %d minutes", hour, mins);
+}
+void decompteTotal(int *hour, int *mins) {
+    *hour = *mins / 60;  // 90 / 60 = 1
+    *mins = *mins % 60; // 90 % 60 = 30
 }
