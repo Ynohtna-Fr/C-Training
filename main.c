9903804522f4ab163d583d86d3a8d0c30b9fc668
Ynohtna <45896947+Ynohtna-Fr@users.noreply.github.com>
2021-07-26 13:47:07 +0200

    [8] create function C_negative

--- main.c
+++ main.c
@@ -5,10 +5,11 @@ void C_print(struct Color c);
 struct Color C_new(int r, int g, int b);
 int clamp(int n);
 struct Color C_multiply(struct Color c, float coef);
+struct Color C_negative(struct Color c);
 
 int main() {
-    Color selectedColor = C_new(255,255,255);
-    Color multipliedColor = C_multiply(selectedColor, 0.2);
+    Color selectedColor = C_new(255,25,0);
+    Color multipliedColor = C_negative(selectedColor);
 
     printf("Hello, World!\n");
 
@@ -44,6 +45,15 @@ struct Color C_multiply(struct Color c, float coef) {
     tmp.red = clamp(c.red * coef);
     tmp.green = clamp(c.green * coef);
     tmp.blue = clamp(c.blue * coef);
+
+    return tmp;
+}
+
+struct Color C_negative(struct Color c) {
+    struct Color tmp;
+    tmp.red = 255 - c.red;
+    tmp.green = 255 - c.green;
+    tmp.blue = 255 - c.blue;
 
     return tmp;
 }
