1ad363a0a803a2e2b05f25781ebe06ef0b28f673
Ynohtna <45896947+Ynohtna-Fr@users.noreply.github.com>
2021-08-03 18:16:47 +0200

    New Exo from PDF -> STRING

---
+++ cmake-build-debug/CMakeFiles/exo_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exo_C.dir/main.c.o"
+  "exo_C"
+  "exo_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exo_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    // ex : 1.1
+    /*
+    for (int i = 0; i < 10; i++) {
+        printf("%d", i);
+    }
+     */
+
+    // ex : 1.2
+//    char cara;
+//    int i = 0;
+//    // the bigget french word is 25 cara long
+//    char word[25];
+//    do {
+//        cara = getchar();
+//        printf("%c", cara);
+//        word[i] = cara;
+//        i++;
+//    } while(cara != EOF && cara != '\n');
+//        word[i] = '\0';
+//
+//    for (i = 0; word[i] != '\0'; i++) {
+//        int num = 0;
+//        if (word[i] == '\n') return 0;
+//
+//        for (int k = 0; k < 25; k++) {
+//            if (word[i] == word[k]) {
+//                num++;
+//            }
+//        }
+//        printf("La lettre %c apparait %d fois \n", word[i], num);
+//    }
+
+//    ex : 1.3
+//    char myChar;
+//    int wordNumber = 0;
+//    while((myChar = getchar()) != EOF && myChar != '\n')
+//    {
+//        if (myChar == ' ') {
+//            wordNumber++;
+//        }
+//    }
+//
+//    printf("Il y a %d Mots dans la phrase", wordNumber);
+
+    return 0;
+}
